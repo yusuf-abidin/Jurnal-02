3d5cc3dd1920bab7dba41accd6b38ad74cea1e29
yusuf-abidin <yusufabidin02@gmail.com>
2020-09-16 11:25:58 +0700

    DataDiri.java

---
+++ Data Diri/DataDiri.java
@@ -0,0 +1,8 @@
+public class DataDiri {
+
+    public static void main(String[] args) {
+	    System.out.println("Nama : Yusuf Abidin Nurrahman");
+		System.out.println("NIM : 6706200070");
+		System.out.println("Kelas : D3IF-44-04");
+	}
+}
